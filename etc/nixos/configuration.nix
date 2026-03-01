c8b58516548858bc21fa2d6b1d2bae04d135fd16
guidx.a <147561800+guida-ngr@users.noreply.github.com>
2026-03-01 16:23:23 -0300

    add | configurate.nix

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, ... }:
+{ imports =[ ./hardware-configuration.nix ];
+
+  system.stateVersion = "25.11";
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  hardware.graphics = { enable = true; enable32Bit = true; };
+
+  services.xserver.enable = true;
+  services.xserver.xkb.layout = "us";
+  services.xserver.windowManager.i3.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.picom.enable = true;
+  services.pipewire = { enable = true; alsa.enable = true; pulse.enable = true; };
+
+  networking.hostName = "nixbtw";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Sao_Paulo";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = { font = "Lat2-Terminus16"; useXkbConfig = true; };
+
+  users.users.guidxa = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio"];
+    packages = with pkgs; [ tree eww nitrogen obsidian ];
+  };
+
+  programs.firefox.enable = true;
+  environment.systemPackages = with pkgs; [
+    kitty
+    neovim
+    git
+    bash
+    jq
+    coreutils
+    socat
+    usbutils
+  ];
+}
